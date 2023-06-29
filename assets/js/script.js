d8f2b8d893f6f7d46f2f889b66fbd66d450021fb
Methawut1996 <methawutjiwanantawat@gmail.com>
2023-06-29 15:34:48 +0700

    update 6/29/2566 15.34

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
-jQuery(document).ready(function($) {
-
-
-});
+jQuery(document).ready(function ($) {
+    console.log(window.location.pathname);
+    var swiper = new Swiper(".my-Swiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      slidesPerView: 1,
+      spaceBetween: 400,
+      loop: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+    });
+  
+  
+  
+  });
