84c78e50b3b0ecf2ec15049c37446dcc2db0bad0
Methawut1996~ <methawutjiwanantawat@gmail.com>
2023-07-03 17:55:46 +0700

    update 3/7/2566 17.55

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
 jQuery(document).ready(function ($) {
-    console.log(window.location.pathname);
-    var swiper = new Swiper(".my-Swiper", {
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      slidesPerView: 1,
-      spaceBetween: 400,
-      loop: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-    });
-  
-  
-  
+  console.log(window.location.pathname);
+  var swiper = new Swiper(".my-Swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    slidesPerView: 1,
+    spaceBetween: 400,
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
   });
+
+  var swiper = new Swiper(".swiper-promotion", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    slidesPerView: 0,
+    spaceBetween: 50,
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 3
+      },
+      1024: {
+        slidesPerView: 4
+      },
+      1560: {
+        slidesPerView: 4
+      }
+    }
+  });
+
+  
+  
+});
